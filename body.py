45892d726cb8d034805a80cc969cf1ddd9f56b6b
kroomDev <gus_the_michael@yahoo.com>
2020-05-09 20:04:33 +0300

    0905

--- body.py
+++ body.py
@@ -5,11 +5,11 @@ body_csc = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 
 #METRISEIS
 im_count =  0 #gia na tsekaro poses eikones anoigo
-ll = []#i lista me ta parathira poy epistrefei o al/mos
+#rectList = []#i lista me ta parathira poy epistrefei o al/mos
 
 def haarCascade(imageFile):
      image = cv2.imread(imageFile)
-     listOfRectangles = []
+     detectRecCoordinates = []
      gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
      body = body_csc.detectMultiScale(gray, 1.3, 1)
      
@@ -17,45 +17,72 @@ def haarCascade(imageFile):
      for (x,y,w,h) in body:
          
          
-         print("I DID IT")
-         listOfRectangles.append(imageFile)
-         listOfRectangles.append(x)
-         listOfRectangles.append(y)
-         listOfRectangles.append(w)
-         listOfRectangles.append(h)
+         #print("I DID IT")
+         #detectRecCoordinates.append(imageFile)
+         detectRecCoordinates.append(x)
+         detectRecCoordinates.append(y)
+         detectRecCoordinates.append(w)
+         detectRecCoordinates.append(h)
          cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),3)
          cv2.imshow('goat',image)
          cv2.waitKey(0)
          
-     return listOfRectangles
+     return detectRecCoordinates
         
 
+def getGtbb(imageFile):
+    nameOfFile = imageFile
+    textFile = open("annot.txt", "r")
+    for line in textFile:
+        if nameOfFile in line: 
+            coord = []
+            step_0 = line.strip()
+            step = step_0.split(' ')
+            for i in range(2, len(step)): 
+                coord.append(int(step[i]))
+    return coord
 
 
 
 #find the body Haar Cascade
 for imageF in glob('*.png'):
      im_count += 1
-         
-     ret = haarCascade(imageF)
-     ll.append(ret)
-     
+     rectList = haarCascade(imageF)
+     realCoord = getGtbb(imageF)
+    # print('The number of rectangle is', rectList)
+    # print(type(rectList))
+    # print('Real coord are', realCoord)
+    # print(type(realCoord))
+     xreal = realCoord[0]
+     yreal = realCoord[1]
+     wreal = realCoord[2]
+     hreal = realCoord[3]
     
-        
+     for j in range(0, len(rectList),  4):
+         xreal = rectList[j]
+         yreal = rectList[j+1]
+         wreal = rectList[j+2]
+         hreal = rectList[j+3]
+         if rectList[j]==realCoord[j%4] and rectList[j+1]==realCoord[(j+1)%4] and rectList[j+2]==realCoord[(j+2)%4] and rectList[j+3]==realCoord[(j+3)%4]:
+             print("HIT")
+         else:
+             print("MISS")
+
+    
+    '''    
 
 #emfanisi eikonas
 
 
 cv2.destroyAllWindows()
 
-
-
-
-
+'''/
 print('The number of images is', im_count)
-print('The number of rectangle is', ll)
+print('The number of rectangle is', rectList)
+print('Real coord are', realCoord)
 
 
 
 
 
+'''
